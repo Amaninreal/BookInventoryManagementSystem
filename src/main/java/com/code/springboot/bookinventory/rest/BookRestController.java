96d983ad21b92cf9b4147c8f120173d084e32c82
Amaninreal <amaninreallife03@gmail.com>
2025-02-28 12:44:34 +0530

    added get list of all books available functionality

--- src/main/java/com/code/springboot/bookinventory/rest/BookRestController.java
+++ src/main/java/com/code/springboot/bookinventory/rest/BookRestController.java
@@ -1,4 +1,30 @@
 package com.code.springboot.bookinventory.rest;
 
+import com.code.springboot.bookinventory.entity.Book;
+import com.code.springboot.bookinventory.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class BookRestController {
+
+    private BookService bookService;
+
+    // inject employee dao layer
+    @Autowired
+    public BookRestController(BookService theBookService){
+        bookService = theBookService;
+    }
+
+    // adding endpoint "/books" to get list of all books
+    @GetMapping("/books")
+    public List<Book> findAll(){
+        return bookService.findAll();
+    }
+
 }
